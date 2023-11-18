5eff8c6383a7819f53b86f774c907e8a4244c6de
Ꮥʜꫝʟɪɴɪ <150043650+Shalini77986@users.noreply.github.com>
2023-11-18 22:14:56 +0530

    Update start.py

--- AarohiX/plugins/start.py
+++ AarohiX/plugins/start.py
@@ -39,7 +39,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAEKvadlUQF_g2kuKBWJ5l7zt6S8IQGP4QAC4QUAAsRkCVfRsAFnpXOVxDME")
+            await message.reply_sticker("CAACAgUAAxkBAAEKuWBlTfbSugdpVxgYHNJ5XoXvE4A4oQAC0QgAAnKqiVWHVdzcCXlegzME")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -84,7 +84,7 @@ async def start_comm(client, message: Message, _):
                     details = stats.get(vidid)
                     title = (details["title"][:35]).title()
                     if vidid == "telegram":
-                        msg += f"🔗[ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ](https://t.me/LOVE_FEELINGS_WILL) ** ᴩʟᴀʏᴇᴅ {count} ᴛɪᴍᴇs**\n\n"
+                        msg += f"🔗[ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ](https://t.me/MUSIC_WORLD_SH) ** ᴩʟᴀʏᴇᴅ {count} ᴛɪᴍᴇs**\n\n"
                     else:
                         msg += f"🔗 [{title}](https://www.youtube.com/watch?v={vidid}) ** played {count} times**\n\n"
                 msg = _["ustats_2"].format(tot, tota, limit) + msg
@@ -124,7 +124,7 @@ async def start_comm(client, message: Message, _):
         if name[0:3] == "del":
             await del_plist_msg(client=client, message=message, _=_)
         if name[0:3] == "inf":
-            m = await message.reply_text("👁️‍🗨️")
+            m = await message.reply_text("🎶")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
@@ -192,7 +192,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAxkBAAEKvadlUQF_g2kuKBWJ5l7zt6S8IQGP4QAC4QUAAsRkCVfRsAFnpXOVxDME")
+                await message.reply_sticker("CAACAgUAAxkBAAEKuWBlTfbSugdpVxgYHNJ5XoXvE4A4oQAC0QgAAnKqiVWHVdzcCXlegzME")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
