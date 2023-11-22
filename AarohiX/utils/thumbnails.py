cbab96f075ac481af0b29480b71209dc4959ee6b
Ꮥʜꫝʟɪɴɪ <150043650+Shalini77986@users.noreply.github.com>
2023-11-22 17:50:35 +0530

    Update thumbnails.py

--- AarohiX/utils/thumbnails.py
+++ AarohiX/utils/thumbnails.py
@@ -124,7 +124,7 @@ async def gen_thumb(videoid):
 
             image4 = ImageDraw.Draw(image2)
             image4.text((10, 10), "SHALINI X MUSIC", fill="blue", font = font1, align ="left") 
-            image4.text((670, 150), "NOW PLAYING By Shalu", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
+            image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
             title1 = truncate(title)
