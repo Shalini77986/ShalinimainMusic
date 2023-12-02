36c38db52ee2f7fa21e29d7797aa095f6057b7f6
Ꮥʜꫝʟɪɴɪ <150043650+Shalini77986@users.noreply.github.com>
2023-12-02 17:44:36 +0530

    Create nstart.py

---
+++ AarohiX/plugins/nstart.py
@@ -0,0 +1,13 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from .. import app
+
+
+
+@app.on_message(filters.command("start") & filters.private & filters.incoming)
+async def f_start(_, message: Message):
+    await message.reply_text(
+        text=f"Hello This is {message.from_user.first_name},\n\n๏ {app.mention},\nTelegram Group Calls Streaming bot with some useful features, written in Python with Pyrogram and Py-Tgcalls. Supporting platforms like Youtube, Spotify, Resso, AppleMusic, Soundcloud and M3u8 Links.",
+        
+    )
