682a5a1131f3b03a92942833e2fe195ff87a3474
Ꮥʜꫝʟɪɴɪ <150043650+Shalini77986@users.noreply.github.com>
2023-12-03 13:09:08 +0530

    Update nstart.py

--- AarohiX/plugins/nstart.py
+++ AarohiX/plugins/nstart.py
@@ -32,7 +32,7 @@ async def str(client, message: Message, _):
                 ],
                 [
                      InlineKeyboardButton(
-                        "ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="settings_back_helper")
+                        "ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="settings_back_helper"),
                 ],
                 [
                     InlineKeyboardButton(
