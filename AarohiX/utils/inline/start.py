05e0ce7f11ba133545730e38423b4fb7ec3c6534
Ꮥʜꫝʟɪɴɪ <150043650+Shalini77986@users.noreply.github.com>
2023-12-24 16:30:09 +0530

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -8,69 +8,47 @@ from AarohiX import app
 
 def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
-        [
-            InlineKeyboardButton(
-                text="➕ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴩ➕",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="🔆ʜᴇʟᴩ🔆",
-                callback_data="settings_back_helper",
-            ),
-            InlineKeyboardButton(
-                text="❤‍🔥sᴇᴛᴛɪɴɢs❤‍🔥", callback_data="settings_helper"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="♻️ Mᴜsɪᴄ Wᴏʀʟᴅ ♻️", url=f"https://t.me/MUSIC_WORLD_SH"),
-            InlineKeyboardButton(
-                text="🌐ᴜᴘᴅᴀᴛᴇs🌐", url=f"https://t.me/ShaliniMusicBotSh"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="💞ᴍᴀɪɴᴛᴀɪɴᴇʀ💞", user_id=OWNER),
-            InlineKeyboardButton(
-                text="✨sᴜᴩᴩᴏʀᴛ✨", url=config.SUPPORT_GROUP
-            ),
-        ],
-     ]
+                [
+                    InlineKeyboardButton(
+                        "★ Add Me ★", url=f"https://t.me/Shalinixmusicbot?startgroup=true")
+                ],
+                [
+                     InlineKeyboardButton(
+                        "ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="settings_back_helper")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/ShaliniMusicBotSh"),
+                    InlineKeyboardButton(
+                        "sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/music_world_sh"),
+                ],    
+                [
+                     InlineKeyboardButton(
+                      "💞ᴍᴀɪɴᴛᴀɪɴᴇʀ💞", url=f"https://t.me/shalini_shalu_69")
+                ]
+    ]
     return buttons
 
 
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
-        [
-            InlineKeyboardButton(
-                text="➕ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴩ➕",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="✨ʜᴇʟᴩ✨", callback_data="settings_back_helper"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="♻️ Mᴜsɪᴄ Wᴏʀʟᴅ ♻️", url=f"https://t.me/MUSIC_WORLD_SH"),
-            InlineKeyboardButton(
-                text="🌐ᴜᴘᴅᴀᴛᴇs🌐", url=f"https://t.me/ShaliniMusicBotSh"
-            ),
-        ],
-        [
-            InlineKeyboardButton(text="💞ᴍᴀɪɴᴛᴀɪɴᴇʀ💞", user_id=OWNER),
-            InlineKeyboardButton(
-                text="🔸sᴜᴩᴩᴏʀᴛ🔹", url=config.SUPPORT_GROUP
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                    text="🔎 ᴏᴡɴᴇʀ 🔍", url=f"https://t.me/Heart_heker_69"
-                )
-        ],
-     ]
+                [
+                    InlineKeyboardButton(
+                        "★ Add Me ★", url=f"https://t.me/Shalinixmusicbot?startgroup=true")
+                ],
+                [
+                     InlineKeyboardButton(
+                        "ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="settings_back_helper")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/ShaliniMusicBotSh"),
+                    InlineKeyboardButton(
+                        "sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/music_world_sh"),
+                ],    
+                [
+                     InlineKeyboardButton(
+                      "💞ᴍᴀɪɴᴛᴀɪɴᴇʀ💞", url=f"https://t.me/shalini_shalu_69")
+                ]
+    ]
     return buttons
